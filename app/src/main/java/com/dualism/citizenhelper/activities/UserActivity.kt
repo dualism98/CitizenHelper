3311355d087256379b9a22870df378343f9e122a
dualism98 <beachsidebearhi@gmail.com>
2021-01-21 23:43:55 +0700

    Added navigation bottom tab

--- app/src/main/java/com/dualism/citizenhelper/activities/UserActivity.kt
+++ app/src/main/java/com/dualism/citizenhelper/activities/UserActivity.kt
@@ -1,15 +1,41 @@
 package com.dualism.citizenhelper.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.dualism.citizenhelper.R
+import kotlinx.android.synthetic.main.activity_user.*
+
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //Remove title bar
-        getSupportActionBar()?.hide();
+        supportActionBar?.hide();
+
         setContentView(R.layout.activity_user)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_themes -> {
+                    setContent("Home")
+                    true
+                }
+                R.id.navigation_requests -> {
+                    setContent("Notification")
+                    true
+                }
+                R.id.navigation_profile -> {
+                    setContent("Search")
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setContent(content: String) {
+        setTitle(content)
+        tvLabel.text = content
     }
 }
--- app/src/main/java/com/dualism/citizenhelper/fragments/auth/FragmentLogin.kt
+++ app/src/main/java/com/dualism/citizenhelper/fragments/auth/FragmentLogin.kt
@@ -1,4 +1,4 @@
-package com.dualism.citizenhelper.fragments
+package com.dualism.citizenhelper.fragments.auth
 
 import android.content.Context
 import android.content.Intent
